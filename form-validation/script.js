98966d27f494a19c9d26e2ed180dce24fc48d136
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2020-07-25 20:33:37 +0530

    validation logic refactor and more rules

--- form-validation/script.js
+++ form-validation/script.js
@@ -4,72 +4,82 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const password2 = document.querySelector('#password2');
 
-username.addEventListener('focusout', (e) => {
-  validateUsername();
-});
-username.addEventListener('input', (e) => {
-  if (username.dataset.error) {
-    validateUsername();
+form.addEventListener('focusout', (e) => {
+  const input = e.target;
+
+  switch (input.id) {
+    case 'username':
+      validateUsername({ min: 3, max: 15 });
+      break;
+    case 'email':
+      validateEmail();
+      break;
+    case 'password':
+      validatePassword({ min: 6 });
+      break;
+    case 'password2':
+      confirmPassword();
+      break;
+    default:
+      break;
   }
 });
 
-email.addEventListener('focusout', (e) => {
-  validateEmail();
-});
-email.addEventListener('input', (e) => {
-  if (email.dataset.error) {
-    validateEmail();
-  }
-});
+form.addEventListener('input', (e) => {
+  const input = e.target;
 
-password.addEventListener('focusout', (e) => {
-  validatePassword();
-});
-password.addEventListener('input', (e) => {
-  if (password.dataset.error) {
-    validatePassword();
-    confirmPassword();
-  }
-
-  if (password.value === '') {
-    password2.disabled = true;
-  }
-
-  if (password.value.length > 0 && password2.disabled) {
-    password2.disabled = false;
-  }
-});
-
-password2.addEventListener('focusout', (e) => {
-  confirmPassword();
-});
-password2.addEventListener('input', (e) => {
-  if (password2.dataset.error) {
-    confirmPassword();
+  if (input.dataset.error === 'true') {
+    switch (input.id) {
+      case 'username':
+        validateUsername({ min: 3, max: 15 });
+        break;
+      case 'email':
+        validateEmail();
+        break;
+      case 'password':
+        validatePassword({ min: 6 });
+        if (password2.dataset.error === 'true') {
+          confirmPassword();
+        }
+        break;
+      case 'password2':
+        confirmPassword();
+        break;
+      default:
+        break;
+    }
   }
 });
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const isUsernameValid = validateUsername();
+  const isUsernameValid = validateUsername({ min: 3, max: 15 });
   const isEmailValid = validateEmail();
-  const isPasswordValid = validatePassword();
+  const isPasswordValid = validatePassword({ min: 6 });
   const isPasswordMatch = confirmPassword();
 
   if (isUsernameValid && isEmailValid && isPasswordValid && isPasswordMatch) {
     console.log('Form submitted');
-    username.value = '';
-    email.value = '';
-    password.value = '';
-    password2.value = '';
-
-    const formGroups = form.querySelectorAll('.form-group');
-    for (let formGroup of formGroups) {
-      formGroup.classList.remove('success');
-    }
+    resetForm();
   }
 });
+
+function resetForm() {
+  username.value = '';
+  username.dataset.error = false;
+  email.value = '';
+  email.dataset.error = false;
+  password.value = '';
+  password.dataset.error = false;
+  password2.value = '';
+  password2.dataset.error = false;
+
+  const formGroups = form.querySelectorAll('.form-group');
+  for (let formGroup of formGroups) {
+    formGroup.classList.remove('success');
+  }
+}
 
 function showError(input, msg) {
   const formGroup = input.parentElement;
@@ -85,11 +95,16 @@ function showSuccess(input) {
   formGroup.classList.add('success');
 }
 
-function validateUsername() {
+function validateUsername({ min, max }) {
   username.dataset.error = true;
 
-  if (username.value.trim() === '') {
-    showError(username, 'Username is required');
+  const value = username.value.trim();
+
+  if (value.length < min) {
+    showError(username, `Username must contain atleast ${min} characters.`);
+    return false;
+  } else if (value.length > max) {
+    showError(username, `Username must not be more than ${max} characters.`);
     return false;
   } else {
     showSuccess(username);
@@ -112,11 +127,15 @@ function validateEmail() {
   }
 }
 
-function validatePassword() {
+function validatePassword({ min }) {
   password.dataset.error = true;
 
-  if (password.value.trim() === '') {
+  const value = password.value.trim();
+  if (value.length === 0) {
     showError(password, 'Password is required');
+    return false;
+  } else if (value.length < min) {
+    showError(password, `Password length must be atleast ${min} characters.`);
     return false;
   } else {
     showSuccess(password);
@@ -128,7 +147,7 @@ function confirmPassword() {
   password2.dataset.error = true;
 
   if (password2.value.trim() === '') {
-    showError(password2, 'Password is required');
+    showError(password2, 'Password2 is required');
     return false;
   } else if (!validator.equals(password.value, password2.value)) {
     showError(password2, 'Passwords do not match');
